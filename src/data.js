36f5c16e2a13352a66d884f2f741de55f6e66323
izanmaizan <izanmaizan@gmail.com>
2024-02-12 14:11:16 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Dimas Saputra',
+        tag: 'dimasmds',
+        imageUrl: 'https://img.freepik.com/free-photo/artist-white_1368-3543.jpg?size=626&ext=jpg',
+      },
+      {
+        id: 2,
+        name: 'Arif Faizin',
+        tag: 'arifaizin',
+        imageUrl: 'https://img.freepik.com/free-photo/beautiful-woman-portrait_144627-27916.jpg?size=626&ext=jpg&ga=GA1.1.2010520287.1707719511&semt=ais',
+      },
+      {
+        id: 3,
+        name: 'Rahmat Fajri',
+        tag: 'rfajri27',
+        imageUrl: 'https://img.freepik.com/free-photo/calm-handsome-curly-haired-boy-posing-isolated-light-grey-standing-still-looks-peaceful-wearing-casual-manner-youth-style-concept_176532-8831.jpg?size=626&ext=jpg&ga=GA1.1.2010520287.1707719511&semt=ais',
+      },
+    ];
+   }
+    
+   export { getData };
